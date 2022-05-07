04507bcc5090a52c0c36884a15e7aea744ce8e9b
danerlt <danerlt001@gmail.com>
2022-05-07 09:38:59 +0800

    Update docker_save.sh

--- docker/docker_save.sh
+++ docker/docker_save.sh
@@ -3,29 +3,51 @@
 # 将docker镜像保存为 项目名.tar.gz文件
 # 使用方式 ./docker_save.sh  image_url
 # image_url格式为 harbor域名/harbor目录/项目名:分支或者标签-docker哈希值-时间-V版本号
-# ai-image.dev.com/neufoundry-private/gateway-outer:origin-feature-xy-Sprint1216-docker-e46882a93e1e3dc5d18b747e088b4d8abdb60cc3-20220225112925-V13
+# ai-image.dev.com/neufoundry-private/gateway-outer:origin-feature-sprint0505-e46882a93e1e3dc5d18b747e088b4d8abdb60cc3-20220225112925-V13
 
-# cd /tmp
+
+
+function usage() {
+    echo "usage: docker_save.sh  iamge_url"
+}
+
+
+if [ $# -ne 1 ]
+then
+    usage && exit 1
+fi
 
 image_url=$1
 
 echo "docker pull ${image_url}"
 docker pull $image_url
 
-array=(${image_url//:/ })
-prefix=${array[0]}
-array2=(${prefix//// })
-image_name=${array2[-1]}
-echo "image_name: $image_name"
+# 从右到左取old_image_path中取第一个:之前的
+image_prefix=${image_url%:*}
+echo "image_prefix: $image_prefix"
 
-tar_name=${image_name}.tar
+# 从左往右取第一个/后面的内容
+harbor_path=${image_prefix#*/}
+echo "harbor_path: $harbor_path"
 
-echo "tar name: $tar_name"
+
+# 从左往右取最后一个/后面的内容 如果包含_ 取下划线后面的内容
+end=${harbor_path##*/}
+if [[ $end == *_* ]]
+then
+    # 从左往右取最后一个 _ 后面的内容
+    project_name=${end##*_}
+else
+    project_name=$end
+fi
+tar_name=${project_name}.tar
+
+echo "tar file name : $tar_name"
 
 echo "docker save $image_url -o $tar_name"
 docker save $image_url -o $tar_name
 
 echo "tar 压缩"
-tar zcvf ${image_name}.tar.gz $tar_name
+tar czvf ${project_name}.tar.gz $tar_name
 
 echo "执行成功"                                         
