7f28f5ee6739422c8beb73855e0617d28d41efb9
李滔 <tao.li67@geely.com>
2022-05-27 10:26:26 +0800

    feat add shell string repace
     Author:    danerlt <danerlt001@gmail.com>

--- shell/string/string.sh
+++ shell/string/string.sh
@@ -4,6 +4,8 @@
 # ${var^^} 转为大写
 #  ${var,,} 转为小写
 # ${var:start:length} 取子串,start从0开始,长度为length
+# ${var/pattern/string} 替换字符串,最长匹配（贪婪匹配）的那部分被 string 替换，但仅替换第一个匹配
+# ${var//pattern/string} 替换字符串,最长匹配（贪婪匹配）的那部分被 string 替换，所有匹配都替换
 
 url="ai-image.dev.com:8080/neufoundry-private/gateway-outer:origin-feature-sprint0505-e46882a93e1e3dc5d18b747e088b4d8abdb60cc3-20220225112925-V13"
 echo "url: ${url}"
@@ -20,3 +22,13 @@ echo "lower_url ${lower_url}"
 
 sub_string=${url:3:5}
 echo "sub_string: ${sub_string}"
+
+
+# 替换第一个 /
+replace_string=${url/\//-}
+echo "replace_string: ${replace_string}"
+
+
+# 替换所有 /
+replace_string=${url//\//-}
+echo "replace_string: ${replace_string}"
