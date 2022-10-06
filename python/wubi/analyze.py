49d86edb7c1ea1d04dbec9adc71ed5730a369fc7
danerlt <danerlt001@gmail.com>
2022-10-06 13:28:08 +0800

    feat 五笔分析

---
+++ python/wubi/analyze.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+"""
+@file: analyze.py 
+@time: 2022-10-06
+@desc: 分析98五笔
+"""
+
+import os
+import opencc
+
+converter = opencc.OpenCC('s2t.json')
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+wubi_98_path = os.path.join(current_dir, "98.txt")
+wubi_86_path = os.path.join(current_dir, "86.txt")
+
+
+def read_map_table(path):
+    """读取码表，返回字典
+
+    :return dict   key: 编码   value: 汉字或词组
+    """
+    result = {}
+    with open(path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line:
+                word, code = line.split("\t")
+                word = word.strip()
+                code = code.strip()
+                result[code] = word
+
+        return result
+
+
+def is_simplified_chinese(word):
+    """是否是简体字
+    :param word: 单字
+    :return True: 是简体字 False：是繁体字
+    """
+    # converter.convert(word) 表示简体字转成繁体字 
+    # 繁体字 调用这个之后还是繁体字
+    return word != converter.convert(word)
+
+
+def statistics(map, map_type="98五笔"):
+    """统计码表"""
+    single_word = []  # 单字
+    simplified_chinese_word = []  # 简体字
+    traditional_chinese_word = []  # 繁体字
+    two_word_phrase = []  # 两字词组
+    three_word_phrase = []  # 三字词组
+    multi_word_phrase = []  # 多字词组
+    sigle_brief = []  # 一级简码
+    two_brief = []  # 二级简码
+    three_brief = []  # 三级简码
+    repeat = []  # 重码
+    sigle_repeat = []  # 单字重码
+    sc_repeat = []  # 简体字重码
+    tc_repeat = []  # 繁体字重码
+    for code, word in map.items():
+        word_len = len(word)
+        code_len = len(code)
+        if word_len == 1:
+            single_word.append(word)
+            if is_simplified_chinese(word):
+                simplified_chinese_word.append(word)
+            else:
+                traditional_chinese_word.append(word)
+        elif word_len == 2:
+            two_word_phrase.append(word)
+        elif word_len == 3:
+            three_word_phrase.append(word)
+        else:
+            multi_word_phrase.append(word)
+
+        if code.endswith("_"):
+            if code_len == 2:
+                sigle_brief.append(word)
+            elif code_len == 3:
+                two_brief.append(word)
+            elif code_len == 4:
+                three_brief.append(word)
+
+        if code_len > 4:
+            repeat.append(word)
+            if word_len == 1:
+                sigle_repeat.append(word)
+                if word in simplified_chinese_word:
+                    sc_repeat.append(word)
+                else:
+                    tc_repeat.append(word)
+
+    print(f"{map_type}单字数量: {len(single_word)}")
+    print(f"{map_type}简体字数量: {len(simplified_chinese_word)}")
+    print(f"{map_type}繁体字数量: {len(traditional_chinese_word)}")
+    print(f"{map_type}两字词组数量: {len(two_word_phrase)}")
+    print(f"{map_type}三字词组数量: {len(three_word_phrase)}")
+    print(f"{map_type}多字词组数量: {len(multi_word_phrase)}")
+    print(f"{map_type}一级简码数量: {len(sigle_brief)}")
+    print(f"{map_type}二级简码数量: {len(two_brief)}")
+    print(f"{map_type}三级简码数量: {len(three_brief)}")
+    print(f"{map_type}重码数量: {len(repeat)}")
+    print(f"{map_type}码表总数量: {len(map)}")
+    print(f"{map_type}重码率: {round(len(repeat) / len(map) * 100, 2)}%")
+    print(f"{map_type}单字重码数量: {len(sigle_repeat)}")
+    print(f"{map_type}简体字重码数量: {len(sc_repeat)}")
+    print(f"{map_type}繁体字重码数量: {len(tc_repeat)}")
+    print(f"{map_type}单字重码率: {round(len(sigle_repeat) / len(single_word) * 100, 2)}%")
+    print(f"{map_type}简体字重码率: {round(len(sc_repeat) / len(simplified_chinese_word) * 100, 2)}%")
+    print()
+
+
+def main():
+    map_98 = read_map_table(wubi_98_path)
+    map_86 = read_map_table(wubi_86_path)
+    statistics(map_98, map_type="98五笔")
+    statistics(map_86, map_type="86五笔")
+
+
+if __name__ == '__main__':
+    main()
