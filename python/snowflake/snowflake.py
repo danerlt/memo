0571d6df1e2d593fb50a9f0f169469ca96f0b4a6
danerlt <danerlt001@gmail.com>
2022-10-14 13:29:17 +0800

    feat python 添加雪花算法

---
+++ python/snowflake/snowflake.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+"""
+@file: snowflake.py 
+@time: 2022-10-14
+@desc: 雪花算法
+"""
+
+# Twitter's Snowflake algorithm implementation which is used to generate distributed IDs.
+# https://github.com/twitter-archive/snowflake/blob/snowflake-2010/src/main/scala/com/twitter/service/snowflake/IdWorker.scala
+
+# 64位
+# 1、最高位占1 bit，值固定为 0，以保证生成的 ID 为正数；
+# 2、中位占 41 bit，值为毫秒级时间戳；
+# 3、中下位占 10 bit，值为工作机器的 ID，值的上限为 1024；
+# 4、末位占 12 bit，值为当前毫秒内生成的不同 ID，值的上限为 4096
+import time
+import logging
+
+# 64位ID的划分
+WORKER_ID_BITS = 5  # 节点位数 范围为0-2^5 即0-31
+DATACENTER_ID_BITS = 5  # 数据中心位数 范围为0-2^5 即0-31
+SEQUENCE_BITS = 12  # 序列号 1毫秒能够生成的id访问为0-2^12 有4096个ID
+
+# 最大取值计算
+MAX_WORKER_ID = -1 ^ (-1 << WORKER_ID_BITS)  # 2**5-1 0b11111
+MAX_DATACENTER_ID = -1 ^ (-1 << DATACENTER_ID_BITS)
+
+# 移位偏移计算
+WOKER_ID_SHIFT = SEQUENCE_BITS
+DATACENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS
+TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTER_ID_BITS
+
+# 序号循环掩码
+SEQUENCE_MASK = -1 ^ (-1 << SEQUENCE_BITS)
+
+# 起始时间
+TWEPOCH = 1288834974657
+
+logger = logging.getLogger('run')
+
+
+class IdWorker(object):
+    """
+    用于生成IDs
+    """
+
+    def __init__(self, datacenter_id, worker_id, sequence=0):
+        """
+        初始化
+        :param datacenter_id: 数据中心（机器区域）ID
+        :param worker_id: 机器ID
+        :param sequence: 起始序号
+        """
+        # sanity check
+        if worker_id > MAX_WORKER_ID or worker_id < 0:
+            raise ValueError('worker_id值越界')
+
+        if datacenter_id > MAX_DATACENTER_ID or datacenter_id < 0:
+            raise ValueError('datacenter_id值越界')
+
+        self.worker_id = worker_id
+        self.datacenter_id = datacenter_id
+        self.sequence = sequence
+
+        self.last_timestamp = -1  # 上次计算的时间戳
+
+    def _gen_timestamp(self):
+        """
+        生成整数时间戳  单位：毫秒
+        :return:int timestamp
+        """
+        return int(time.time() * 1000)
+
+    def get_id(self):
+        """
+        获取新ID
+        :return:
+        """
+        timestamp = self._gen_timestamp()
+
+        # 时钟回拨
+        if timestamp < self.last_timestamp:
+            msg = f"clock is moving backwards. Rejecting requests until {self.last_timestamp}"
+            logger.error(msg)
+            raise Exception(msg)
+
+        if timestamp == self.last_timestamp:
+            self.sequence = (self.sequence + 1) & SEQUENCE_MASK
+            if self.sequence == 0:
+                timestamp = self._til_next_millis(self.last_timestamp)
+        else:
+            self.sequence = 0
+
+        self.last_timestamp = timestamp
+
+        new_id = ((timestamp - TWEPOCH) << TIMESTAMP_LEFT_SHIFT) | (self.datacenter_id << DATACENTER_ID_SHIFT) | \
+                 (self.worker_id << WOKER_ID_SHIFT) | self.sequence
+        return new_id
+
+    def _til_next_millis(self, last_timestamp):
+        """
+        等到下一毫秒
+        """
+        timestamp = self._gen_timestamp()
+        while timestamp <= last_timestamp:
+            timestamp = self._gen_timestamp()
+        return timestamp
+
+
+# 单例
+id_work = IdWorker(1, 2, 0)
+
+
+def gen_global_id():
+    """生成全区ID"""
+    return id_work.get_id()
