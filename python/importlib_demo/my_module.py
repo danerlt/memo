8719819fc2416798f1d34daa4a026b637326ca4a
danerlt <danerlt001@gmail.com>
2023-06-28 11:10:05 +0800

    Update my_module.py

--- python/importlib_demo/my_module.py
+++ python/importlib_demo/my_module.py
@@ -8,5 +8,18 @@
 @desc: 
 """
 
+
 def my_function():
     print('Hello, world!')
+
+
+l = [2, 3, 1, 4, 5]
+s = set(l)
+print(s)
+s.add(10)
+print(s)
+s.add(0)
+print(s)
+
+
+from collections import OrderedDict
